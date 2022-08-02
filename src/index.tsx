6b5b70af3ee60818ee0b11bd6906f76c5e08e3ea
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-08-02 10:45:12 +0200

    fix: remove unused bootstrap import from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import './scss/styles.scss';
 
-import AppDemo from './tsx/AppDemo';
 import './scss/styles_main.scss';
+// import * as bootstrap from 'bootstrap'
+
+import MainDashboard from './tsx/MainDashboard';
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
-root.render(<AppDemo />);
+root.render(<MainDashboard />);
