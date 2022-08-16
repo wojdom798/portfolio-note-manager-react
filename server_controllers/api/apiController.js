3b4c9b31cde7831fd1ca4e26b66146aba9664913
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-08-16 18:03:42 +0200

    feat: hook up category routes to main api controller

--- server_controllers/api/apiController.js
+++ server_controllers/api/apiController.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const __projectDir = path.join(__dirname, "../../");
 
 const notesMainController = require("./notes/notesMainController");
+const categoriesMainController = require("./categories/categoriesMainController");
 
 router.use("/notes", notesMainController);
+router.use("/categories", categoriesMainController);
 
 module.exports = router;
