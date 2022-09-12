167fd7f064d25104a7114600fdbff42b08a68a9f
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-09-12 17:56:44 +0200

    test express-session

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+const session = require("express-session");
+const Sequelize = require("sequelize");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db.sqlite3"
+});
+
+
+async function testDbSequelize()
+{
+  try
+  {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  }
+  catch (error)
+  {
+    console.log("unable to connect to the database: ", error);
+  }
+}
+
+// sequelize.close();
+testDbSequelize();
+
+
 const app = express();
 const portNumber = process.env.PORT || 8001;
 
@@ -13,6 +40,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
+
+app.use(
+  session({
+    secret: "secret message",
+    store: new SequelizeStore({ db: sequelize }),
+    resave: false,
+    proxy: true
+  }),
+);
 
 app.get('/', function(req, res, next) {
     console.log(`Request: [${req.method}], `, req.originalUrl);
