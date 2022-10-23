f99ac39f64597050d514cd812c537c4025c63be8
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-10-23 21:36:52 +0200

    refactor: implement SQL queries for postgresql in getDateRange, getCategories, and getTags routes

--- routes/api/categories/getCategoriesRoute.js
+++ routes/api/categories/getCategoriesRoute.js
@@ -11,9 +11,15 @@ const projectSettingsFileName = "project_settings.json";
 const settingsFilePath = path.join(__projectDir, projectSettingsFileName);
 const projectSettings = JSON.parse(fs.readFileSync(settingsFilePath, "utf8"));
 
+const passport = require("passport");
+
+const { Pool, Client } = require("pg");
+const postgresPool = new Pool(projectSettings.database.postgresql);
 
 // route: /api/categories/get
 router.get("/",
+// passport.authenticate("local", { failureRedirect: "/api/auth/logout" }),
+// passport.authenticate("local", { failureRedirect: "/api/auth/unauthorized" }),
 async function (req, res)
 {
     let currentTime = new Date().toLocaleString("pl-PL",{ hour12: false });
@@ -23,44 +29,85 @@ async function (req, res)
     let query = "";
     let queryResult;
 
-    try
+    // let userId = req.session.passport.user.id;
+    let userId = 1;
+
+    if (projectSettings.database.selected_database === "sqlite")
     {
-        sqlite3.verbose();
-        const databaseHandle = await createDbConnection(
-            path.join(__projectDir, projectSettings.database.filename));
-        
-        queryArray = 
-        [
-            `SELECT * FROM category `,
-            `WHERE user_id = ${req.session.passport.user.id} `,
-            `ORDER BY id ASC;`
-        ];
-        for (let line of queryArray)
+        try
         {
-            query += line;
-        }
-
-        queryResult = await databaseHandle.all(query);
-        await databaseHandle.close();
-
-        res.json({  
-            responseMsg: "Success",
-            responseData: {
-                categories: queryResult
+            sqlite3.verbose();
+            const databaseHandle = await createDbConnection(
+                path.join(__projectDir, projectSettings.database.sqlite.filename));
+            
+            queryArray = 
+            [
+                `SELECT * FROM category `,
+                `WHERE user_id = ${req.session.passport.user.id} `,
+                `ORDER BY id ASC;`
+            ];
+            for (let line of queryArray)
+            {
+                query += line;
             }
-        });
-    
+
+            queryResult = await databaseHandle.all(query);
+            await databaseHandle.close();
+
+            res.json({  
+                responseMsg: "Success",
+                responseData: {
+                    categories: queryResult
+                }
+            });
+        }
+        catch (error)
+        {
+            console.error(error);
+            res.status(404);
+            // res.send("<h1>404</h1>");
+            res.json({
+                responseMsg: "An error occured during the querying of data.",
+                errorMsg: error.message
+            });
+        }
     }
-    catch (error)
+    else if (projectSettings.database.selected_database === "postgresql")
     {
-        console.error(error);
-        res.status(404);
-        // res.send("<h1>404</h1>");
-        res.json({
-            responseMsg: "An error occured during the querying of data.",
-            errorMsg: error.message
-        });
-    }
+        try
+        {
+            queryArray = 
+            [
+                `SELECT *, TO_CHAR(date_added, 'YYYY-MM-DD HH24:MI:SS') AS date_added `,
+                `FROM category `,
+                `WHERE user_id = ${userId} `,
+                `ORDER BY id ASC;`
+            ];
+            for (let line of queryArray)
+            {
+                query += line;
+            }
+
+            queryResult = (await postgresPool.query(query)).rows;
+
+            res.json({  
+                responseMsg: "Success",
+                responseData: {
+                    categories: queryResult
+                }
+            });
+        }
+        catch (error)
+        {
+            console.error(error);
+            res.status(404);
+            // res.send("<h1>404</h1>");
+            res.json({
+                responseMsg: "An error occured during the querying of data.",
+                errorMsg: error.message
+            });
+        }
+    } // end: else - database = postgres
 }); // end route
 
 
--- routes/api/getDateRangeRoute.js
+++ routes/api/getDateRangeRoute.js
@@ -13,6 +13,9 @@ const projectSettings = JSON.parse(fs.readFileSync(settingsFilePath, "utf8"));
 
 const passport = require("passport");
 
+const { Pool, Client } = require("pg");
+const postgresPool = new Pool(projectSettings.database.postgresql);
+
 // route: /api/get-date-range
 router.get("/",
 // passport.authenticate("local", { failureRedirect: "/api/auth/unauthorized" }),
@@ -25,47 +28,95 @@ async function (req, res)
     let query = "";
     let queryResult;
 
-    try
+    // let userId = req.session.passport.user.id;
+    let userId = 1;
+
+    if (projectSettings.database.selected_database === "sqlite")
     {
-        sqlite3.verbose();
-        const databaseHandle = await createDbConnection(
-            path.join(__projectDir, projectSettings.database.filename));
-        
-        queryArray = 
-        [
-            `SELECT `,
-            `MIN(nt.date_added) AS min_date, `,
-            `MAX(nt.date_added) AS max_date `,
-            `FROM note nt `,
-            `WHERE user_id = ${req.session.passport.user.id};`
-        ];
-        for (let line of queryArray)
+        try
         {
-            query += line;
-        }
-
-        queryResult = await databaseHandle.all(query);
-        await databaseHandle.close();
-
-        res.json({  
-            responseMsg: "Success",
-            responseData: {
-                minDate: queryResult[0].min_date,
-                maxDate: queryResult[0].max_date,
+            sqlite3.verbose();
+            const databaseHandle = await createDbConnection(
+                path.join(__projectDir, projectSettings.database.sqlite.filename));
+            
+            queryArray = 
+            [
+                `SELECT `,
+                `MIN(nt.date_added) AS min_date, `,
+                `MAX(nt.date_added) AS max_date `,
+                `FROM note nt `,
+                `WHERE user_id = ${userId};`
+            ];
+            for (let line of queryArray)
+            {
+                query += line;
             }
-        });
-    
+
+            queryResult = await databaseHandle.all(query);
+            await databaseHandle.close();
+
+            res.json({  
+                responseMsg: "Success",
+                responseData: {
+                    minDate: queryResult[0].min_date,
+                    maxDate: queryResult[0].max_date,
+                }
+            });
+        
+        }
+        catch (error)
+        {
+            console.error(error);
+            res.status(404);
+            // res.send("<h1>404</h1>");
+            res.json({
+                responseMsg: "An error occured during the querying of data.",
+                errorMsg: error.message
+            });
+        }
     }
-    catch (error)
+    else if (projectSettings.database.selected_database === "postgresql")
     {
-        console.error(error);
-        res.status(404);
-        // res.send("<h1>404</h1>");
-        res.json({
-            responseMsg: "An error occured during the querying of data.",
-            errorMsg: error.message
-        });
+        try
+        {
+            queryArray = 
+            [
+                `SELECT `,
+                `TO_CHAR(MIN(nt.date_added), 'YYYY-MM-DD HH24:MI:SS') AS min_date, `,
+                `TO_CHAR(MAX(nt.date_added), 'YYYY-MM-DD HH24:MI:SS') AS max_date `,
+                `FROM note nt `,
+                `WHERE user_id = 1; `
+            ];
+            for (let line of queryArray)
+            {
+                query += line;
+            }
+
+            queryResult = (await postgresPool.query(query)).rows;
+
+            res.json({  
+                responseMsg: "Success",
+                responseData: {
+                    minDate: queryResult[0].min_date,
+                    maxDate: queryResult[0].max_date,
+                }
+            });
+        
+        }
+        catch (error)
+        {
+            console.error(error);
+            res.status(404);
+            // res.send("<h1>404</h1>");
+            res.json({
+                responseMsg: "An error occured during the querying of data.",
+                errorMsg: error.message
+            });
+        }
     }
+
+
+    
 }); // end route
 
 
