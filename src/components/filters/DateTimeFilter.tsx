23873297dfb429079829ea00d03d375bd1ea1371
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-11-01 20:56:15 +0100

    refactor: move reusable functions from DateTimeFilter.tsx to helper.ts

--- src/components/filters/DateTimeFilter.tsx
+++ src/components/filters/DateTimeFilter.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+// Type imports
+// import { Date } from "../../types";
+
 // Redux imports
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { selectFilters, setDateRangeFilter } from "../../redux/filterSlice";
@@ -22,6 +25,8 @@ import MuiCheckbox from "@mui/material/Checkbox"
 import MuiFormControlLabel from "@mui/material/FormControlLabel"
 import ButtonBase from "@mui/material/ButtonBase";
 
+// helper functions/utils imports
+import helper from "../../helper";
 
 interface Date
 {
@@ -30,162 +35,45 @@ interface Date
     day: number;
 };
 
+const months = [
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+];
+
+const daysOfTheWeek = {
+    en: [
+        { long: "Monday", short: "M" },
+        { long: "Tuesday", short: "T" },
+        { long: "Wednesday", short: "W" },
+        { long: "Thursday", short: "T" },
+        { long: "Friday", short: "F" },
+        { long: "Saturday", short: "S" },
+        { long: "Sunday", short: "S" }
+    ]
+};
 
 function DateTimeFilter()
 {
     const dispatch = useAppDispatch();
     const filters = useAppSelector(selectFilters);
-    const [selectedYear, setSelectedYear] = useState((new Date()).getFullYear());
-    const [selectedMonth, setSelectedMonth] = useState(1);
-    const [selectedDay, selectDay] = useState<number>(1);
+    const [activeYear, setActiveYear] = useState((new Date()).getFullYear());
+    const [activeMonth, setActiveMonth] = useState(1);
+    const [activeDay, setActiveDay] = useState<number>(1);
     const [isSelectingEndDateActive, setSelectingEndDateActive] = useState<boolean>(false);
-
-    // temporary
-    // const [selectedStartDate, setSelectedStartDate] = useState<string>("---");
-    // const [selectedEndDate, setSelectedEndDate] = useState<string>("---");
+    
     const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(null);
     const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(null);
-
     const [draftDateStart, setDraftDateStart] = useState<Date | null>(null);
     const [draftDateEnd, setDraftDateEnd] = useState<Date | null>(null);
 
     // modal
     const [isDateRangePickerModalOpen, setDateRangePickerModalOpen] = useState<boolean>(false);
 
-    const months = [
-        "January", "February", "March", "April", "May", "June", "July",
-        "August", "September", "October", "November", "December"
-    ];
-    
-    const daysOfTheWeek = {
-        en: [
-            { long: "Monday", short: "M" },
-            { long: "Tuesday", short: "T" },
-            { long: "Wednesday", short: "W" },
-            { long: "Thursday", short: "T" },
-            { long: "Friday", short: "F" },
-            { long: "Saturday", short: "S" },
-            { long: "Sunday", short: "S" }
-        ]
-    };
-
     useEffect(() =>
     {
-        setSelectedStartDate(filters.dateRange ? convertStringToDate(filters.dateRange!.start) : null);
-        setSelectedEndDate(filters.dateRange ? convertStringToDate(filters.dateRange!.end) : null);
+        setSelectedStartDate(filters.dateRange ? helper.convertStringToDate(filters.dateRange!.start) : null);
+        setSelectedEndDate(filters.dateRange ? helper.convertStringToDate(filters.dateRange!.end) : null);
     }, [filters]);
-
-    function convertStringToDate(date: string): Date
-    {
-        const dateStr = date.split(" ")[0] as string;
-        const splitDateStr = dateStr.split("-");
-
-        if (splitDateStr.length === 1)
-        {
-            // console.log("splitDateStr.length = 1");
-            return {
-                year: parseInt(splitDateStr[0].slice(0, 4)),
-                month: parseInt(splitDateStr[0].slice(4, 6)),
-                day: parseInt(splitDateStr[0].slice(6))
-            };
-        }
-        else
-        {
-            return {
-                year: parseInt(splitDateStr[0]),
-                month: parseInt(splitDateStr[1]),
-                day: parseInt(splitDateStr[2])
-            };
-        }
-    }
-
-    function isLeapYear(year: number)
-    {
-        return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
-    }
-
-    function getDaysInYear(year: number)
-    {
-        return isLeapYear(year) ? 366 : 365;
-    }
-
-    function getNumberOfDaysInMonth(year: number, month: number)
-    {
-        const daysOfMonthArray: number[] = [
-            31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-        ];
-        return daysOfMonthArray[month-1];
-    }
-
-    /**
-     * computes and returns the number of the first day of the week for a given month and year
-     * Monday = `1`, Tuesday = `2`, ..., Sunday = `7`
-     * @param {number} year
-     * @param {number} month
-     */
-    function fdm(year: number, month: number)
-    {
-        const baseDate =
-        {
-            month: 1,
-            year: 2000,
-            fdm: 6
-        };
-        const DAYS_IN_A_WEEK = 7;
-        let yearDifference = year - baseDate.year;
-        let monthDiff: number;
-        const isLeap = isLeapYear(year);
-        const daysOfMonthArray: number[] = [
-            31, isLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-        ];
-        let sumOfDays: number = baseDate.fdm;
-
-        if (yearDifference === 0)
-        {
-            if (month === baseDate.month) return baseDate.fdm;
-                monthDiff = month - baseDate.month;
-            if (monthDiff < 0) throw new Error("Month difference is less than 0.");
-            for (let i = 0; i < monthDiff; i++)
-            {
-                sumOfDays += daysOfMonthArray[i];
-            }
-            return (sumOfDays % DAYS_IN_A_WEEK) === 0 ? DAYS_IN_A_WEEK : sumOfDays % DAYS_IN_A_WEEK;
-        }
-        else if (yearDifference > 0)
-        {
-            sumOfDays = baseDate.fdm;
-            for (let i = 0; i < yearDifference; i++)
-                sumOfDays += getDaysInYear(baseDate.year + i);
-            for (let i = 0; i < month-1; i++)
-                sumOfDays += daysOfMonthArray[i];
-            return (sumOfDays % DAYS_IN_A_WEEK) === 0 ? DAYS_IN_A_WEEK : sumOfDays % DAYS_IN_A_WEEK;
-        }
-        else
-        {
-            yearDifference = Math.abs(yearDifference);
-            sumOfDays = baseDate.fdm;
-            for (let i = 1; i < yearDifference; i++)
-            {
-                if (isLeapYear(baseDate.year - i))
-                    sumOfDays -= 2; // 366 % 7 == 2
-                else
-                    sumOfDays -= 1; // 365 % 7 == 1
-            }
-            for (let i = daysOfMonthArray.length-1; i >= month-1; i--)
-            {
-                sumOfDays -= daysOfMonthArray[i];
-            }
-            // return (7 - Math.abs(sumOfDays-1) % 7) + 1;
-            return ((((sumOfDays-1) % DAYS_IN_A_WEEK) + DAYS_IN_A_WEEK) % DAYS_IN_A_WEEK) +1;
-        }
-        return -1;
-    }
-
-    const handleDebugBtnClick = () =>
-    {
-        const firstDay = fdm(selectedYear, selectedMonth);
-        console.log(`${months[selectedMonth-1]}, ${selectedYear}; fdm = ${firstDay}`);
-    };
 
     function getMonthMenuItems()
     {
@@ -193,52 +81,6 @@ function DateTimeFilter()
         {
             return (<MuiMenuItem key={index} value={index+1}>{monthName}</MuiMenuItem>);
         });
-    }
-
-    function handleMonthSelectChange(event: any)
-    {
-        setSelectedMonth(event.target.value);
-    }
-
-    function handleIncreaseMonth()
-    {
-        if (selectedMonth + 1 > 12)
-        {
-            // setSelectedMonth(12);
-            setSelectedMonth(1);
-            setSelectedYear(selectedYear+1);
-        }
-        else
-            setSelectedMonth(selectedMonth + 1);
-    }
-
-    function handleDecreaseMonth()
-    {
-        if (selectedMonth - 1 < 1)
-        {
-            // setSelectedMonth(1);
-            setSelectedMonth(12);
-            setSelectedYear(selectedYear-1);
-        }
-        else
-            setSelectedMonth(selectedMonth - 1);
-    }
-
-    function handleIncreaseYear()
-    {
-        setSelectedYear(selectedYear + 1);
-    }
-
-    function handleDecreaseYear()
-    {
-        setSelectedYear(selectedYear - 1);
-    }
-
-    function changeYearInput(event: any)
-    {
-        const yearFromInput = Number(event.target.value);
-        if (!isNaN(yearFromInput))
-            setSelectedYear(yearFromInput);
     }
 
     function getDayNameElements()
@@ -265,7 +107,7 @@ function DateTimeFilter()
         let classStr: string = "";
         for (let i = 0; i < count; i++)
         {   
-            if (i+1 === selectedDay)
+            if (i+1 === activeDay)
                 classStr = "day day-number active";
             else if (i+1 === (new Date).getDate())
                 classStr = "day day-number today";
@@ -289,13 +131,70 @@ function DateTimeFilter()
         return dayElements;
     }
 
+
+    //***************************************************************
+    //                      Event Handlers
+    //***************************************************************
+    const handleDebugBtnClick = () =>
+    {
+        const firstDay = helper.fdm(activeYear, activeMonth);
+        console.log(`${months[activeMonth-1]}, ${activeYear}; fdm = ${firstDay}`);
+        console.log("debug - cleaning up the component");
+    };
+
+    function handleMonthSelectChange(event: any)
+    {
+        setActiveMonth(event.target.value);
+    }
+
+    function handleIncreaseMonth()
+    {
+        if (activeMonth + 1 > 12)
+        {
+            // setActiveMonth(12);
+            setActiveMonth(1);
+            setActiveYear(activeYear+1);
+        }
+        else
+            setActiveMonth(activeMonth + 1);
+    }
+
+    function handleDecreaseMonth()
+    {
+        if (activeMonth - 1 < 1)
+        {
+            // setActiveMonth(1);
+            setActiveMonth(12);
+            setActiveYear(activeYear-1);
+        }
+        else
+            setActiveMonth(activeMonth - 1);
+    }
+
+    function handleYearInputChange(event: any)
+    {
+        const yearFromInput = Number(event.target.value);
+        if (!isNaN(yearFromInput))
+            setActiveYear(yearFromInput);
+    }
+
+    function handleIncreaseYear()
+    {
+        setActiveYear(activeYear + 1);
+    }
+
+    function handleDecreaseYear()
+    {
+        setActiveYear(activeYear - 1);
+    }
+
     function handleDayClick(dayNumber: number)
     {
-        selectDay(dayNumber);
+        setActiveDay(dayNumber);
         const newDate: Date =
         {
-            year: selectedYear,
-            month: selectedMonth,
+            year: activeYear,
+            month: activeMonth,
             day: dayNumber
         };
         if (!isSelectingEndDateActive)
@@ -312,28 +211,6 @@ function DateTimeFilter()
         }
     }
 
-    // temporary
-    function formatDateString(year: number, month: number, day: number, separator = "")
-    {
-        let str: string = "";
-        let yearStr = String(year);
-        let monthStr = month >= 10 ? String(month) : `0${String(month)}`;
-        let dayStr = day >= 10 ? String(day) : `0${String(day)}`;
-        str = `${yearStr}${separator}${monthStr}${separator}${dayStr}`;
-        return str
-    }
-
-    function formatDateString2(date: Date | null, separator = "")
-    {
-        if (date == null) return "null";
-        let str: string = "";
-        let yearStr = String(date.year);
-        let monthStr = date.month >= 10 ? String(date.month) : `0${String(date.month)}`;
-        let dayStr = date.day >= 10 ? String(date.day) : `0${String(date.day)}`;
-        str = `${yearStr}${separator}${monthStr}${separator}${dayStr}`;
-        return str
-    }
-
     function handleDateRangePickerApplyBtnClick()
     {
         // const dateRange = {
@@ -341,16 +218,15 @@ function DateTimeFilter()
         //     end: "20220822"
         // };
         const dateRange = {
-            // start: formatDateString2(selectedStartDate),
-            // end: formatDateString2(selectedEndDate)
-            start: formatDateString2(draftDateStart),
-            end: formatDateString2(draftDateEnd)
+            // start: helper.formatDateString(selectedStartDate),
+            // end: helper.formatDateString(selectedEndDate)
+            start: helper.formatDateString(draftDateStart),
+            end: helper.formatDateString(draftDateEnd)
         };
         // console.log(dateRange);
         dispatch(setDateRangeFilter(dateRange));
     }
 
-    // modal
     function handleShowDateRangePickerModal()
     {
         setDateRangePickerModalOpen(true);
@@ -360,12 +236,6 @@ function DateTimeFilter()
     {
         setDateRangePickerModalOpen(false);
     }
-
-    function handleDateRangePickerApplyChangesBtnClick()
-    {
-
-    }
-    // end: modal
 
     return (
         <React.Fragment>
@@ -377,9 +247,9 @@ function DateTimeFilter()
         <div className="date-range-picker-init-container">
             <div className="wrapper-1">
             <div className="wrapper-2">
-                <span className="daterange-start">{selectedStartDate != null ? formatDateString2(selectedStartDate, "-") : "null"}</span>
+                <span className="daterange-start">{selectedStartDate != null ? helper.formatDateString(selectedStartDate, "-") : "null"}</span>
                 <span> - </span>
-                <span className="daterange-end">{selectedEndDate != null ? formatDateString2(selectedEndDate, "-") : "null"}</span>
+                <span className="daterange-end">{selectedEndDate != null ? helper.formatDateString(selectedEndDate, "-") : "null"}</span>
             </div>
             </div>
             <div className="calendar-icon-container">
@@ -410,7 +280,7 @@ function DateTimeFilter()
                             <MuiSelect
                                 labelId="select-month-label"
                                 id="select-month"
-                                value={selectedMonth}
+                                value={activeMonth}
                                 onChange={(event: any) => { handleMonthSelectChange(event) }}
                             >
                                 { getMonthMenuItems() }
@@ -422,9 +292,9 @@ function DateTimeFilter()
                             onClick={handleIncreaseMonth}
                             variant="outlined">&#x3E;</MuiButton>
                         <MuiTextField
-                            onChange={changeYearInput}
+                            onChange={handleYearInputChange}
                             label="Year"
-                            value={selectedYear}    
+                            value={activeYear}    
                         />
                         <MuiButtonGroup
                             orientation="vertical"
@@ -442,9 +312,9 @@ function DateTimeFilter()
                     <div className="day-container">
                         { getDayNameElements() }
                         {/* { insertEmptyDayElements(5) } */}
-                        { insertEmptyDayElements(fdm(selectedYear, selectedMonth)-1) }
+                        { insertEmptyDayElements(helper.fdm(activeYear, activeMonth)-1) }
                         {/* { insertDayElements(31) } */}
-                        { insertDayElements(getNumberOfDaysInMonth(selectedYear, selectedMonth)) }
+                        { insertDayElements(helper.getNumberOfDaysInMonth(activeYear, activeMonth)) }
                     </div>
                 </div>
                 <div className="datetime-filter-footer">
@@ -461,8 +331,8 @@ function DateTimeFilter()
                         variant="contained"
                     >debug</MuiButton>
                     <div style={ {display: "block"} }>
-                        <p>start date: {draftDateStart != null ? formatDateString2(draftDateStart, "-") : "null"}</p>
-                        <p>end date: {draftDateEnd != null ? formatDateString2(draftDateEnd, "-") : "null"}</p>
+                        <p>start date: {draftDateStart != null ? helper.formatDateString(draftDateStart, "-") : "null"}</p>
+                        <p>end date: {draftDateEnd != null ? helper.formatDateString(draftDateEnd, "-") : "null"}</p>
                     </div>
                 </div>
             </div>
--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,11 @@
+// import { Date } from "./types";
+interface Date
+{
+    year: number;
+    month: number;
+    day: number;
+};
+
 export default (function()
 {
 
@@ -14,8 +22,141 @@ function getCurrentDateTimeString(): string
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
+function isLeapYear(year: number)
+{
+    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+}
+
+function getDaysInYear(year: number)
+{
+    return isLeapYear(year) ? 366 : 365;
+}
+
+function getNumberOfDaysInMonth(year: number, month: number)
+{
+    const daysOfMonthArray: number[] = [
+        31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ];
+    return daysOfMonthArray[month-1];
+}
+
+/**
+ * computes and returns the number representing the first day of the week for a given month and year
+ * Monday = `1`, Tuesday = `2`, ..., Sunday = `7`
+ * @param {number} year
+ * @param {number} month
+ */
+function fdm(year: number, month: number)
+{
+    const baseDate =
+    {
+        month: 1,
+        year: 2000,
+        fdm: 6
+    };
+    const DAYS_IN_A_WEEK = 7;
+    let yearDifference = year - baseDate.year;
+    let monthDiff: number;
+    const isLeap = isLeapYear(year);
+    const daysOfMonthArray: number[] = [
+        31, isLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ];
+    let sumOfDays: number = baseDate.fdm;
+
+    if (yearDifference === 0)
+    {
+        if (month === baseDate.month) return baseDate.fdm;
+            monthDiff = month - baseDate.month;
+        if (monthDiff < 0) throw new Error("Month difference is less than 0.");
+        for (let i = 0; i < monthDiff; i++)
+        {
+            sumOfDays += daysOfMonthArray[i];
+        }
+        return (sumOfDays % DAYS_IN_A_WEEK) === 0 ? DAYS_IN_A_WEEK : sumOfDays % DAYS_IN_A_WEEK;
+    }
+    else if (yearDifference > 0)
+    {
+        sumOfDays = baseDate.fdm;
+        for (let i = 0; i < yearDifference; i++)
+            sumOfDays += getDaysInYear(baseDate.year + i);
+        for (let i = 0; i < month-1; i++)
+            sumOfDays += daysOfMonthArray[i];
+        return (sumOfDays % DAYS_IN_A_WEEK) === 0 ? DAYS_IN_A_WEEK : sumOfDays % DAYS_IN_A_WEEK;
+    }
+    else
+    {
+        yearDifference = Math.abs(yearDifference);
+        sumOfDays = baseDate.fdm;
+        for (let i = 1; i < yearDifference; i++)
+        {
+            if (isLeapYear(baseDate.year - i))
+                sumOfDays -= 2; // 366 % 7 == 2
+            else
+                sumOfDays -= 1; // 365 % 7 == 1
+        }
+        for (let i = daysOfMonthArray.length-1; i >= month-1; i--)
+        {
+            sumOfDays -= daysOfMonthArray[i];
+        }
+        // return (7 - Math.abs(sumOfDays-1) % 7) + 1;
+        return ((((sumOfDays-1) % DAYS_IN_A_WEEK) + DAYS_IN_A_WEEK) % DAYS_IN_A_WEEK) +1;
+    }
+    return -1;
+}
+
+function convertStringToDate(date: string): Date
+{
+    const dateStr = date.split(" ")[0] as string;
+    const splitDateStr = dateStr.split("-");
+
+    if (splitDateStr.length === 1)
+    {
+        // console.log("splitDateStr.length = 1");
+        return {
+            year: parseInt(splitDateStr[0].slice(0, 4)),
+            month: parseInt(splitDateStr[0].slice(4, 6)),
+            day: parseInt(splitDateStr[0].slice(6))
+        };
+    }
+    else
+    {
+        return {
+            year: parseInt(splitDateStr[0]),
+            month: parseInt(splitDateStr[1]),
+            day: parseInt(splitDateStr[2])
+        };
+    }
+}
+
+// temporary
+function formatDateString(year: number, month: number, day: number, separator = "")
+{
+    let str: string = "";
+    let yearStr = String(year);
+    let monthStr = month >= 10 ? String(month) : `0${String(month)}`;
+    let dayStr = day >= 10 ? String(day) : `0${String(day)}`;
+    str = `${yearStr}${separator}${monthStr}${separator}${dayStr}`;
+    return str
+}
+
+function formatDateString2(date: Date | null, separator = "")
+{
+    if (date == null) return "null";
+    let str: string = "";
+    let yearStr = String(date.year);
+    let monthStr = date.month >= 10 ? String(date.month) : `0${String(date.month)}`;
+    let dayStr = date.day >= 10 ? String(date.day) : `0${String(date.day)}`;
+    str = `${yearStr}${separator}${monthStr}${separator}${dayStr}`;
+    return str
+}
+
+
 return {
-    getCurrentDateTimeString: getCurrentDateTimeString
+    getCurrentDateTimeString: getCurrentDateTimeString,
+    fdm: fdm,
+    getNumberOfDaysInMonth: getNumberOfDaysInMonth,
+    convertStringToDate: convertStringToDate,
+    formatDateString: formatDateString2
 };
 
 })();
