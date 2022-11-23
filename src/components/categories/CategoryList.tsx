92830f234893c5ff11a3fe6e82f71448bdf6b21c
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-11-23 18:30:57 +0100

    feat: redesign category list, render categories in a table

--- src/components/categories/CategoryList.tsx
+++ src/components/categories/CategoryList.tsx
@@ -1,13 +1,11 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, SyntheticEvent } from "react";
 
 // Redux imports
-import { store } from "../../redux/store";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import {
     add as addCategory,
     edit as editCategory,
     remove as deleteCategory,
-    fetchCategories,
     selectCategoryList
 } from "../../redux/categorySlice";
 
@@ -19,9 +17,7 @@ import CategoryForm from "./CategoryForm";
 
 // Bootstrap imports
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Card from "react-bootstrap/Card";
-
+import BootstrapModal from 'react-bootstrap/Modal';
 
 
 function CategoryList(props: any)
@@ -30,6 +26,7 @@ function CategoryList(props: any)
     const dispatch = useAppDispatch();
     const [showModal, setShowModal] = useState(false);
     const [categoryToEdit, setCategoryToEdit] = useState<ICategory | null>(null);
+    const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
 
     useEffect(() =>
     {
@@ -44,23 +41,74 @@ function CategoryList(props: any)
 
     const handleCloseModal = () => setShowModal(false);
 
-    function categoriesToElement()
+    const handleFormClose = () =>
     {
-        return Object.values(categories).map((item: any) =>
+        setCategoryToEdit(null);
+        setShowModal(false);
+    };
+
+    const handleToggleAllItemSelection = (event: SyntheticEvent) =>
+    {
+        if ((event.target as HTMLInputElement).checked)
+        {
+            const allIds: number[] = [];
+            Object.values(categories).forEach(({id}: ICategory) =>
+            {
+                allIds.push(id)
+            });
+            setSelectedCategories(allIds);
+        }
+        else
+        {
+            setSelectedCategories([]);
+        }
+    }
+
+    const handleToggleItemSelection = (itemId: number) =>
+    {
+        if (selectedCategories.includes(itemId))
+        {
+            setSelectedCategories(selectedCategories.filter((currentItem) =>
+            {
+                return currentItem !== itemId;
+            }));
+        }
+        else
+        {
+            setSelectedCategories([...selectedCategories, itemId]);
+        }
+    }
+
+    function renderCategoriesAsTableRows(categories: ICategory[])
+    {
+        return categories.map(
+            ({ id, name, date_added }: ICategory, index: number) =>
         {
             return (
-            <Card key={item.id} border="primary" style={{ width: '18rem' }}>
-                <Card.Header>{item.date_added}</Card.Header>
-                <Card.Body>
-                <Card.Title>{item.name}</Card.Title>
-                <Button
-                    onClick={() => { handleEditCategoryButtonClick(item.id) }}
-                    variant="primary">edit</Button>
-                <Button
-                    onClick={ () => { handleDeleteCategoryButtonClick(item.id); }}
-                    variant="danger">delete</Button>
-                </Card.Body>
-            </Card>
+                <tr
+                    className={
+                        selectedCategories.includes(id) ?
+                        "table-view__row table-view__row--active" : "table-view__row"
+                    }
+                    key={id}
+                    onClick={() => {handleToggleItemSelection(id)}}
+                >
+                    <td className="table-view__column">
+                        <input
+                            checked={selectedCategories.includes(id)}
+                            type="checkbox"
+                            onChange={() => {handleToggleItemSelection(id)}}/>
+                    </td>
+                    <td className="table-view__column">{index+1}</td>
+                    {/* <td>{id}</td> */}
+                    <td
+                        className={
+                            "table-view__column " +
+                            "table-view__column--horizontally-scrollable"
+                        }
+                    >{name}</td>
+                    <td className="table-view__column">{date_added}</td>
+                </tr>
             );
         });
     }
@@ -69,6 +117,34 @@ function CategoryList(props: any)
     {
         setCategoryToEdit(categories[categoryId]);
         setShowModal(true);
+    }
+
+    async function deleteCategoriesDB(categoryIds: number[])
+    {
+        let url;
+        if (categoryIds.length === 1)
+            url = `api/categories/delete/${categoryIds[0]}`;
+        else
+            url = `api/categories/delete/${JSON.stringify(categoryIds)}`;
+        
+        const init = {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try
+        {
+            const response = await fetch(url, init);
+            dispatch(deleteCategory(categoryIds));
+        }
+        catch (error: any)
+        {
+            console.log(
+                "Error [CategoryForm.tsx, handleDeleteCategoryButtonClick()]: ",
+                error.message
+            );
+        }
     }
 
     async function handleDeleteCategoryButtonClick(categoryId: number)
@@ -87,7 +163,10 @@ function CategoryList(props: any)
         }
         catch (error: any)
         {
-            console.log("Error [CategoryForm.tsx, handleDeleteCategoryButtonClick()]: ", error.message);
+            console.log(
+                "Error [CategoryForm.tsx, handleDeleteCategoryButtonClick()]: ",
+                error.message
+            );
         }
     }
 
@@ -111,7 +190,10 @@ function CategoryList(props: any)
             }));
         })
         .catch(err => {
-            console.log("Error [CategoryForm.tsx, handleFormSubmit()]: ", err.message);
+            console.log(
+                "Error [CategoryForm.tsx, handleFormSubmit()]: ",
+                err.message
+            );
         });
     }
 
@@ -132,7 +214,10 @@ function CategoryList(props: any)
         }
         catch (error: any)
         {
-            console.log("Error [CategoryForm.tsx, handleEditCategoryFormSubmit()]: ", error.message);
+            console.log(
+                "Error [CategoryForm.tsx, handleEditCategoryFormSubmit()]: ",
+                error.message
+            );
         }
         finally
         {
@@ -140,89 +225,109 @@ function CategoryList(props: any)
             setShowModal(false);
         }
     }
-    
-    if (!categoryToEdit)
-    {
-        return (
-        <Fragment>
-            <Fragment>
-                {categoriesToElement()}
-                <Button
-                    onClick={handleShowModal}
-                    variant="primary"
-                    className="floating-action-btn-round"
-                    type="button">
-                <span>&#x2B;</span>
-                </Button>
-            </Fragment>
-    
-            {/* modal dialog */}
-            <Modal
-                show={showModal}
-                onHide={handleCloseModal}
-                backdrop="static"
-                keyboard={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-    
-                <Modal.Body>
-                    <CategoryForm onFormSubmit={handleAddNewCategoryFormSubmit} />
-                </Modal.Body>
-    
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-                    <Button variant="primary" onClick={handleCloseModal}>Save changes</Button>
-                </Modal.Footer>
-            </Modal>
-            {/* end: modal dialog */}
-        </Fragment>
-        );
-    }
-    else
-    {
-        return (
-        <Fragment>
-            <Fragment>
-                {categoriesToElement()}
-                <Button
-                    onClick={handleShowModal}
-                    variant="primary"
-                    className="floating-action-btn-round"
-                    type="button"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal">
-                <span>&#x2B;</span>
-                </Button>
-            </Fragment>
-    
-            {/* modal dialog */}
-            <Modal
-                show={showModal}
-                onHide={handleCloseModal}
-                backdrop="static"
-                keyboard={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
 
-                <Modal.Body>
-                    {/* display edit form */}
+    const handleEditActionClick = () =>
+    {
+        if (selectedCategories.length === 1)
+        {
+            setCategoryToEdit(categories[selectedCategories[0]]);
+            setShowModal(true);
+        }
+    }
+
+    const handleDeleteActionClick = () =>
+    {
+        console.log("deleting categories: ");
+        console.log(selectedCategories);
+
+        deleteCategoriesDB(selectedCategories);
+        setSelectedCategories([]);
+    }
+
+    return (
+    <Fragment>
+        <div className="category-list">
+            {/* {categoriesToElement()} */}
+            <h2 className="category-list__title">Categories</h2>
+            <div className="category-list__button-container">
+                <button
+                    className="notes-app-btn"
+                    disabled={selectedCategories.length !== 1}
+                    onClick={handleEditActionClick}
+                >Edit</button>
+                <button
+                    className="notes-app-btn notes-app-btn--danger"
+                    disabled={selectedCategories.length === 0}
+                    onClick={handleDeleteActionClick}
+                >Delete</button>
+                {/* <button
+                    className="notes-app-btn"
+                    onClick={() => console.log(selectedCategories)}
+                >log selected categories (DBG)</button> */}
+            </div>
+            <div className="category-list__table-container">
+            <table className="table-view">
+                <thead className="table-view__header">
+                    <tr className="table-view__header-row">
+                        <th className="table-view__column table-view__column--header">
+                            <input
+                                disabled={Object.values(categories).length === 0}
+                                checked={
+                                    Object.values(categories).length !== 0 &&
+                                    Object.values(categories).length === 
+                                    selectedCategories.length
+                                }
+                                type="checkbox"
+                                onChange={handleToggleAllItemSelection}/>
+                        </th>
+                        <th
+                            className="table-view__column table-view__column--header"
+                        >#</th>
+                        {/* <th>ID</th> */}
+                        <th
+                            className="table-view__column table-view__column--header"
+                        >Name</th>
+                        <th
+                            className="table-view__column table-view__column--header"
+                        >Date Added</th>
+                    </tr>
+                </thead>
+                <tbody className="table-view__body">
+                    { renderCategoriesAsTableRows(Object.values(categories)) }
+                </tbody>
+            </table>
+            </div>
+
+            <Button
+                onClick={handleShowModal}
+                variant="primary"
+                className="floating-action-btn-round"
+                type="button">
+            <span>&#x2B;</span>
+            </Button>
+        </div>
+
+        <BootstrapModal
+            show={showModal}
+            backdrop="static"
+            keyboard={false}>
+            <BootstrapModal.Body>
+                { categoryToEdit ? (
                     <CategoryForm
                         categoryToEdit={categoryToEdit}
                         onFormSubmit={handleEditCategoryFormSubmit}
+                        onFormClose={handleFormClose}
                     />
-                </Modal.Body>
-
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-                    <Button variant="primary" onClick={handleCloseModal}>Save changes</Button>
-                </Modal.Footer>
-            </Modal>
-            {/* end: modal dialog */}
-        </Fragment>
-        );
-    }
+                ) : (
+                    <CategoryForm
+                        onFormSubmit={handleAddNewCategoryFormSubmit}
+                        onFormClose={handleFormClose}
+                    />
+                )}
+            </BootstrapModal.Body>
+        </BootstrapModal>
+    </Fragment>
+    );
 }
 
 export default CategoryList;
