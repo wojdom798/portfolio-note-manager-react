f6ae8ec2c2d4b67096142101336cb9b9afe44999
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2022-11-28 12:09:29 +0100

    feat: redesign multi action button - rectangular shape

--- src/components/notes/MultiActionButton.tsx
+++ src/components/notes/MultiActionButton.tsx
@@ -7,26 +7,38 @@ import { MultiActionButtonProps } from "../../types";
 import { IonIcon } from "react-ion-icon";
 
 
-function MultiActionButton({ onEditAction, onDeleteAction, onManageTagsAction }: MultiActionButtonProps)
+function MultiActionButton({
+    onEditAction,
+    onDeleteAction,
+    onManageTagsAction }: MultiActionButtonProps)
 {
     const [isActionListActive, setIsActionListActive] = useState<boolean>(false);
 
     return (
         <div className="multi-action-btn-container">
             <button
+                // className="multi-action-btn multi-action-btn--round"
                 className="multi-action-btn"
                 onClick={() => setIsActionListActive(!isActionListActive)}
             >
                 <IonIcon name="ellipsis-vertical-outline" />
             </button>
 
-            <div className={isActionListActive ? "action-list-container active": "action-list-container"}>
+            <div
+                className={isActionListActive ?
+                    "action-list-container active" :
+                    "action-list-container"
+                }
+            >
                 <button
                     className="multi-action-btn__action"
                     onClick={onEditAction}
                 >Edit</button>
                 <button
-                    className="multi-action-btn__action multi-action-btn__action--warning"
+                    className={
+                        "multi-action-btn__action " +
+                        "multi-action-btn__action--warning"
+                    }
                     onClick={onDeleteAction}
                 >Delete</button>
                 <button
